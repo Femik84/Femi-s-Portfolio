4a2fee194c889e6eb2c194165bf6204a46232019
Femik84 <femik84@gmail.com>
2026-01-17 18:57:18 +0100

    Updated

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -40,42 +40,45 @@ import Image from "next/image";
 const TestimonialSlider = () => {
   return (
     <Swiper
-       navigation={true}
+      navigation={true}
       pagination={{ clickable: true }}
       modules={[Navigation, Pagination]}
       className="h-[400px]"
+      allowTouchMove={true}
+      speed={600}
     >
       {testimonialData.map((person, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="flex flex-col items-center md:flex-row gap-x-8  h-full px-16 ">
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
               {/* avatar, name, position */}
-              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0 ">
-                <div className="flex flex-col justify-center text-center  ">
+              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div className="flex flex-col justify-center text-center">
                   {/* avatar */}
-                  <div className="mb-2 mx-auto  "> 
+                  <div className="mb-2 mx-auto"> 
                     <Image
-                    src={person.image}
-                    width={100}
-                    height={100}
-                    alt=""
+                      src={person.image}
+                      width={100}
+                      height={100}
+                      alt=""
+                      priority={index === 0}
                     />
                   </div>
                   {/* name */}
-                  <div className="text-lg ">{person.name}</div>
+                  <div className="text-lg">{person.name}</div>
                   {/* postion */}
-                  <div className="text-[12px] uppercase font-extralight tracking-widest "> {person.position}</div>
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">{person.position}</div>
                 </div>
               </div>
 
               {/* quote & message */}
-              <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20 ">
+              <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
                 {/* quote icon */}
                 <div className="mb-4">
-                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0 " />
+                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
                 </div>
                 {/* message */}
-                <div className="xl:text-lg text-[14px] text-center md:text-left ">
+                <div className="xl:text-lg text-[14px] text-center md:text-left">
                   {person.message}
                 </div>
               </div>
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -7,8 +7,8 @@ import {fadeIn} from '../../variants'
 
 const Testimonials = () => {
   return (
-    <div className="h-full bg-primary/30 py-32 text-center  "> 
-      <div className="container mx-auto h-full flex flex-col justify-center ">
+    <div className="h-full bg-primary/30 py-32 text-center"> 
+      <div className="container mx-auto h-full flex flex-col justify-center">
          {/* title */}
         <motion.h2 
          variants={fadeIn("up", 0.2)}
@@ -16,7 +16,7 @@ const Testimonials = () => {
             animate="show"
             exit="hidden"
         className="h2 mb-8 xl:mb-0"> 
-           What clients <span className="text-accent "> say.</span>
+           What clients <span className="text-accent"> say.</span>
         </motion.h2>
 
          {/* slider */}
