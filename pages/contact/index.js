807984e3f8fa2a150f28045010e73a6673253c86
Femik84 <femik84@gmail.com>
2026-01-17 23:19:53 +0100

    Updated

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -65,14 +65,14 @@ const Contact = () => {
 
   return (
     <div className="h-full bg-primary/30">
-      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+      <div className="container mx-auto py-32 relative bottom-4  xl:text-left flex items-center justify-center h-full">
         <div className="flex flex-col w-full max-w-[700px]">
           <motion.h2
-            variants={fadeIn("up", 0.2)}
+            variants={fadeIn("up", 0.4)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h2 text-[34px] text-center mb-8"
+            className="h2 text-[36px]  mb-6"
           >
             Let&apos;s <span className="text-accent">connect.</span>
           </motion.h2>
@@ -131,7 +131,7 @@ const Contact = () => {
 
             <button
               type="submit"
-              className="btn rounded-full border border-white/50 max-w-[130px] px-4 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group relative"
+              className="btn rounded-full border border-white/50 max-w-[120px] px-4 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group relative"
               disabled={isSending || buttonText === "Message sent!"}
             >
               <span
