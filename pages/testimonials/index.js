2319f8c71121b459e39407a47de71faff93c9414
Femik84 <femik84@gmail.com>
2026-01-17 22:56:44 +0100

    Updated

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,4 +1,3 @@
-//components
 import TestimonialSlider from "../../components/TestimonialSlider";
 
 //framer motion
@@ -7,14 +6,8 @@ import {fadeIn} from '../../variants'
 
 const Testimonials = () => {
   return (
-    <motion.div 
-      className="h-full bg-primary/30 py-32 text-center overflow-hidden"
-      initial={{ opacity: 1 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    > 
-      <div className="container mx-auto h-full flex flex-col justify-center">
+    <div className="h-full bg-primary/30 py-32 text-center  "> 
+      <div className="container mx-auto h-full flex flex-col justify-center ">
          {/* title */}
         <motion.h2 
          variants={fadeIn("up", 0.2)}
@@ -22,12 +15,12 @@ const Testimonials = () => {
             animate="show"
             exit="hidden"
         className="h2 mb-8 xl:mb-0"> 
-           What clients <span className="text-accent"> say.</span>
+           What clients <span className="text-accent "> say.</span>
         </motion.h2>
 
          {/* slider */}
          <motion.div 
-           variants={fadeIn("up", 0.4)}
+           variants={fadeIn("up", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
@@ -35,7 +28,7 @@ const Testimonials = () => {
            <TestimonialSlider />
          </motion.div>
       </div>
-    </motion.div>
+    </div>
   )
 };
 
