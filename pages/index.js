396b629e4c094e69d59781b0444d38ed73713d23
Femik84 <femik84@gmail.com>
2026-02-09 19:32:11 +0100

    Updated

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col relative xshort:bottom-0 short:bottom-40 justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col relative xshort:bottom-0 short:bottom-10 justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
             variants={titleVariants}
